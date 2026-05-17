a1f59b990c7a034f04b246ab61897ee6a25b66c8
talati09 <stalati04@gmail.com.com>
2026-05-17 18:40:37 +0530

    3rd edit

--- parser.cpp
+++ parser.cpp
@@ -5,6 +5,8 @@ vector<Command> parse(vector<Token> tokens) {
     vector<Command> commands;
     Command current;
 
+    current.appendOutput=false; //default;
+
     for(int i = 0; i < tokens.size(); i++) {
         Token t = tokens[i];
 
@@ -15,6 +17,27 @@ vector<Command> parse(vector<Token> tokens) {
                 current.args.push_back(t.value);
             }
         }
+        else if(t.type==REDIR_OUT){
+            i++;// move to next toekn the file name
+            current.outputFile=tokens[i].value;
+            current.appendOutput=false;
+        }
+       
+        // >> append redirect
+        else if(t.type==REDIR_APPEND){
+            i++;  // move to next token (the filename)
+            current.outputFile = tokens[i].value;
+            current.appendOutput = true;
+        }
+        
+
+        //input redirect
+
+        else if(t.type==REDIR_IN){
+            i++;
+            current.inputFile=tokens[i].value;
+        }
+
         else if(t.type == PIPE) {
             commands.push_back(current);
             current = Command();
