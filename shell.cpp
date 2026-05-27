4db5b38d9f5802a897f7761dcf46ea91ba0b14cf
talati09 <stalati04@gmail.com>
2026-05-27 18:42:06 +0530

    signals thing updated

--- shell.cpp
+++ shell.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <signal.h>        // ← add this for signals
 #include "tokenizer.h"
 #include "parser.h"
 #include "builtins.h"
-#include "executor.h" 
-#include <unistd.h>     // ← add this
+#include "executor.h"
 using namespace std;
+
+// this function runs when Ctrl+C is pressed
+// and no command is running
+void handleSIGINT(int sig) {
+    cout << "\nmysh> ";
+    flush(cout);
+}
 
 int main() {
     string input;
 
+    // tell shell to call handleSIGINT when Ctrl+C pressed
+    signal(SIGINT, handleSIGINT);
+
     while(true) {
         char cwd[1024];
         getcwd(cwd, sizeof(cwd));
-        cout << "mysh> "<<cwd<<": ";
+        cout << "mysh:" << cwd << "> ";
         flush(cout);
 
         if(!getline(cin, input)) {
@@ -28,27 +39,31 @@ int main() {
             break;
         }
 
-        // tokenize
         vector<Token> tokens = tokenize(input);
-
-        // parse
         vector<Command> commands = parse(tokens);
 
-        // execute
-         // execute
-     if(commands.size() == 1) {
-    // single command
-    bool wasBuiltin = runBuiltin(commands[0]);
-    if(!wasBuiltin) {
-        executeCommand(commands[0]);
-    }
-   }
-     else if(commands.size() > 1) {
-    // pipeline of commands
-    executePipeline(commands);
-   }
+        if(commands.size() == 1) {
+            bool wasBuiltin = runBuiltin(commands[0]);
+            if(!wasBuiltin) {
+                // ignore SIGINT in parent while child runs
+                signal(SIGINT, SIG_IGN);
+
+                executeCommand(commands[0]);
+
+                // restore handler after child finishes
+                signal(SIGINT, handleSIGINT);
+            }
+        }
+        else if(commands.size() > 1) {
+            // ignore SIGINT in parent while pipeline runs
+            signal(SIGINT, SIG_IGN);
+
+            executePipeline(commands);
+
+            // restore handler after pipeline finishes
+            signal(SIGINT, handleSIGINT);
+        }
     }
 
     return 0;
 }
-//checking comment---------//
